5ac4704b22fbe88393a4eaf6ea1720f2366936b8
shumail <shumail.aaren@gmail.com>
2025-03-29 17:41:37 +0530

    all react code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Simple React App!</h1>
+        <p>Deployed through Jenkins!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
